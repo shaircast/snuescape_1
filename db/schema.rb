a163514813b8f3593fadba2e4e242616f3110380
Sunghwan Park <shaircast@gmail.com>
2017-04-13 06:13:27 +0000

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170412234011) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tables", force: :cascade do |t|
     t.integer  "date"
     t.integer  "time"
